2d5ad10ca9f44de8aa495ca4081e4c9d3373900e
kyle nunery <kyle.nunery@gmail.com>
2016-07-12 10:44:44 -0400

    added basic todo items endpoint

---
+++ Feature/Todos/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+namespace Feature.Todos
+{
+    public class TodoItem
+    {
+        public string Key {get; set;}
+        public string Name { get; set;}
+        public bool IsComplete {get;set;}
+    }  
+}
---
+++ Feature/Todos/TodoController.cs
@@ -0,0 +1,66 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Feature.Todos
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+        public ITodoRepository TodoItems { get; set; }
+
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+        
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtAction("GetTodo", new { id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            TodoItems.Remove(id);
+        }
+    }
+}
