d31d1b7d5b76e0d2005e9b2a488fee3a756a877a
kyle nunery <kyle.nunery@gmail.com>
2016-07-23 12:15:10 -0600

    don't version ping controller

--- PingController.cs
+++ PingController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 [AllowAnonymous]
 public class PingController
 {
-    [HttpGet("api/v1/ping")]
+    [HttpGet("api/ping")]
     public object Ping(){
         return  DateTime.UtcNow;
     }
